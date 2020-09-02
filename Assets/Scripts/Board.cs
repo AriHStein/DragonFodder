25b4f02b21127953be4f8faf7ebbb593a4bf98b2
Ari Stein <ariel.h.stein@gmail.com>
2020-09-02 16:21:38 -0700

    First commit. Starting board

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public List<Square> Squares;
+    public Square GetSquareAt(int row, int column)
+    {
+        return Squares[row * width + column];
+    }
+}
